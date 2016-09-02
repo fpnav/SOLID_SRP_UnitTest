e2b8b9dec851446470621917c6085523311de5ea
fpnav <fpnav@yahoo.com.br>
2016-09-02 09:05:13 -0300

    web angular

---
+++ WebAngular/App/Controllers/AlunoController.js
@@ -0,0 +1,11 @@
+﻿angular.module("app")
+    .controller("AlunoController", function () {
+
+
+        $scope.aluno = {
+            Id: 1,
+            Nome: "JOAO ALEBERTO"
+        };
+
+
+    });
